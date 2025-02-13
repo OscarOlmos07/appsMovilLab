6eeab52c992d74e15749a5451946f78818c27757
OscarOlmos07 <oscarolmos173@gmail.com>
2025-02-12 19:08:46 -0600

    Update Actividad5.swift

---
+++ swift/Actividad 4/Actividad5.swift
@@ -0,0 +1,51 @@
+print("Assignment Operator")
+let b = 10 
+var a = 5
+
+
+a = b // a is now equal to 10 
+print("Variable a: ", a)
+
+print("Tupla")
+let (x, y) = (1, 2)
+print("El valor de x: ", x)
+print("El valor de y: ", y)
+// x is equal to 1, and y is equal to 2 
+
+print("Arithmetic Operators")
+print("1 + 2 = ", 1 + 2)
+print("5 - 3 = ", 5 - 3)
+print("2 * 3 = ", 2 * 3)
+print("10.0 / 2.5 = ", 10.0 / 2.5)
+print("hello" + "wolrd")
+
+print("Compound Assignment")
+var myScore = 10 
+myScore += 3 
+print("Adds 3 to myScore: ", myScore)
+myScore -= 5
+print("Substract 5 from myScore: ", myScore)
+myScore *= 2 
+print("Multiples myScore by 2: ", myScore)
+myScore /= 2
+print("Divides myScore by 2: ", myScore)
+
+print("Reminder Operator")
+print("El reminder operator de 9 % 4: " , 9 % 4)
+
+
+print("Order of Operations")
+var c = 2
+var d = 3
+var e = 5
+print("c + d * e: ", (c + d * e))
+print("(c+d) * e: ", ((c + d) * e))
+
+print("Numeric type version")
+let x2 = 3 
+let y3 = 0.1415927
+print("El valor de pi es: ", (x + y))
+
+let x3 = 3 
+let y4 = 0.1415927
+print("El valor de pi es: ", (Double(x3) + y4))
